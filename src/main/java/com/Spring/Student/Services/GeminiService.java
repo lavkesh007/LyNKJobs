ad9be910c967269425f196f9915c52ecaab06a2c
MacBook <macbook@MacBooks-MacBook-Air.local>
2026-04-20 09:13:52 +0530

    fix

--- src/main/java/com/Spring/Student/Services/GeminiService.java
+++ src/main/java/com/Spring/Student/Services/GeminiService.java
@@ -38,7 +38,8 @@ public class GeminiService {
             ObjectMapper mapper = new ObjectMapper();
 
             Map<String, Object> request = new HashMap<>();
-            request.put("model", "llama3-70b-8192");
+//            request.put("model", "llama3-70b-8192");
+            request.put("model", "llama-3.1-8b-instant");
 
             List<Map<String, String>> messages = new ArrayList<>();
             Map<String, String> msg = new HashMap<>();
