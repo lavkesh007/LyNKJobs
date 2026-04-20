0e970ae46b54c0928acffe0c5a2c97e059f967e1
MacBook <macbook@MacBooks-MacBook-Air.local>
2026-04-20 11:50:43 +0530

    fix

--- src/main/java/com/Spring/Student/Controller/MCQController.java
+++ src/main/java/com/Spring/Student/Controller/MCQController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ import jakarta.servlet.http.HttpServletRequest;
 
 
 @RestController
+@CrossOrigin(origins = "*") 
 @RequestMapping("/mcqs")
 public class MCQController {
 	@Autowired
